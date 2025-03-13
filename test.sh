a4ffd26becc41e0178f96210a19994768cf7d873
stowu2005 <stowu2005@gmail.com>
2025-03-12 23:49:19 -0700

    test.sh easy to make

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+data="_all"
+
+case $1 in 
+    "chi")
+    data="_chinese"
+    ;;
+    "ger")
+    data="_german"
+    ;;
+    "hin")
+    data="_hindi"
+    ;;
+    "rus")
+    data="_russian"
+    ;;
+    "eng")
+    data="_english"
+    ;;
+    esac
+    
+python3 src/myprogram.py test --test_data test_data/input/input$data.txt --test_output pred$data.txt
+python3 grader/grade.py pred$data.txt test_data/answer/answer$data.txt --verbose
