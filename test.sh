07f932877f95b3874164014710fb42224ee5f919
stowu2005 <stowu2005@gmail.com>
2025-03-13 00:25:27 -0700

    Added arabic and hebrew to script and time it took to run

--- test.sh
+++ test.sh
@@ -25,7 +25,19 @@ case $1 in
     "ita")
     data="_italian"
     ;;
+    "heb")
+    data="_hebrew"
+    ;;
+    "ara")
+    data="_arabic"
+    ;;
     esac
-    
+
+
+start=$SECONDS
 python3 src/myprogram.py test --test_data test_data/input/input$data.txt --test_output pred$data.txt
+duration=$(( SECONDS - start ))
 python3 grader/grade.py pred$data.txt test_data/answer/answer$data.txt --verbose
+echo Model took $duration seconds to run
+
+
